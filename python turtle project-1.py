154e5ee9c649f22841dc120c811c991ab877753f
Chandrika shekhar <96369349+chandrika017@users.noreply.github.com>
2023-01-03 01:33:47 +0600

    Add files via upload

---
+++ python turtle project-1.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(0)
+for i in range(6):
+    for colours in ("red","blue","green","yellow","cyan","purple"):
+        turtle.color(colours)
+        turtle.circle(100)
+        turtle.left(10)
+turtle.hideturtle()
